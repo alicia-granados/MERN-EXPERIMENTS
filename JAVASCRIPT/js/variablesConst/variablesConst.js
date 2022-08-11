0affe00ba882f0c0a9db2dacf16960f11a90180a
alicia-granados <aliciagranados35@gmail.com>
2022-08-11 11:58:08 -0500

    feat: :sparkles: Javascript : Variables const

---
+++ JAVASCRIPT/js/variablesConst/variablesConst.js
@@ -0,0 +1,14 @@
+/* variables ->  const */ 
+
+const cliente = "juan";
+const precioDescuento = 220 // camelCase
+
+
+console.log(cliente)
+console.log(precioDescuento)
+
+/* NOTA : NO SE PUEDE RE ASIGNAR, tampoco se puede iniciar sin un valor  marcara error
+cliente = 20
+const precio
+precio = 100
+*/
