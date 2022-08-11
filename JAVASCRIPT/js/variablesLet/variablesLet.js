60955321aa1972c117d7f752e340c049ff3adf97
alicia-granados <aliciagranados35@gmail.com>
2022-08-11 11:50:17 -0500

    feat: :sparkles: Javascript : Variables let

---
+++ JAVASCRIPT/js/variablesLet/variablesLet.js
@@ -0,0 +1,16 @@
+// variables -> var, let, const
+
+let cliente = "juan";
+let precioDescuento = 220 // camelCase
+
+console.log(cliente)
+console.log(precioDescuento)
+
+// Re Asignar
+cliente = 20
+console.log(cliente)
+
+// Pueden iniciar sin un valor
+let precio
+precio = 100
+console.log (precio)
