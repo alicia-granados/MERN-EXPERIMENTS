0e851947cfe9a5989088f8751179046550b65391
alicia-granados <aliciagranados35@gmail.com>
2022-08-11 13:19:47 -0500

    feat: :sparkles: Javascript: Arrays

---
+++ JAVASCRIPT/js/Arrays/arrays.js
@@ -0,0 +1,10 @@
+// Arrays o Arreglos
+
+const tecnologias = [20,true, "react"]
+
+console.log(tecnologias)
+console.table(tecnologias)
+
+console.log(tecnologias[2])
+console.log(tecnologias.length)
+console.log(tecnologias.toString())
