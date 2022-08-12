5f25847e0c487ef8e70ba1c7a199a7a4d6089a13
alicia-granados <aliciagranados35@gmail.com>
2022-08-12 15:13:32 -0500

    feat: :sparkles: Javascript: Imports y Exports

---
+++ JAVASCRIPT/js/DOM/funciones.js
@@ -0,0 +1,14 @@
+const suma = (num1,num2= 0) => {
+    return num1+ num2
+}
+
+const resta = (num1,num2= 0) => {
+    return num1- num2
+}
+
+
+//exporta solo una funcion export default suma
+// exportar varias funciones
+export{
+    suma,resta
+}
---
+++ JAVASCRIPT/js/DOM/importsExports.js
@@ -0,0 +1,7 @@
+// Imports y Exports
+
+// import nombrarComoQuieras from './funciones.js' no importa si solo se exporta una funcion
+
+import { suma , resta } from './funciones.js'
+const resultado = suma (20,30)
+console.log(resultado)
