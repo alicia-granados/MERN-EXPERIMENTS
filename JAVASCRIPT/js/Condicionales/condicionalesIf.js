620f2be7512c2e34cffb0b7a73fd7bb36688efad
alicia-granados <aliciagranados35@gmail.com>
2022-08-12 12:49:22 -0500

    feat: :sparkles: Javascript: Condicional -> If

---
+++ JAVASCRIPT/js/Condicionales/condicionalesIf.js
@@ -0,0 +1,13 @@
+// Condicionales -> IF
+
+const disponible = 4000
+const retirar = 200
+
+if(disponible > retirar){
+    //se cumple la condición y se ejecuta el código 
+    console.log("si puedes retirar")
+}else{
+    // no se cumpla la condición, se ejecuta este código
+    console.log("lo siento intente más tarde")
+}
+
