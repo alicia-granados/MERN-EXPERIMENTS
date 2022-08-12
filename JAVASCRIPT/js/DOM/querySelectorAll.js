9cda0169b2541f66ff746e2710a213de69ca7863
alicia-granados <aliciagranados35@gmail.com>
2022-08-12 13:46:13 -0500

    feat: :sparkles: Javascript:  Selectores del DOM->  query Selector All

---
+++ JAVASCRIPT/js/DOM/querySelectorAll.js
@@ -0,0 +1,8 @@
+// Selectores del DOM->  query Selector All
+
+const heading3 = document.querySelector(".heading")
+console.log(heading3.textContent)
+
+// retorna mas de 1 elemento
+const enlaces = document.querySelectorAll(".navegacion a")
+console.log(enlaces)
