607b9962d24bfdfcce46e320636a6529523e1bbd
alicia-granados <aliciagranados35@gmail.com>
2022-08-13 16:55:45 -0500

    feat: :sparkles: React: Creando y llamando nuestro primer Componente

--- Proyecto 1/citas_React/src/App.jsx
+++ Proyecto 1/citas_React/src/App.jsx
@@ -1,34 +1,11 @@
+import Header from "./components/Header"
 function App() {
-  
-  const sumar = () => {
-    const numero = 5;
-    if(numero > 5){
-      console.log("si es mayor")
-    }else{
-      console.log("no es mayir")
-    }
-    console.log(2 + 2)
-  }
-  sumar();
-
-  const edad = 18;
 
   return (
     <>
-      { 1 + 1 }
-      {edad >= 18 ? 'eres mayor de edad' : 'no eres mayor de edad'}
-      
-      <div >
-        <h1>{"Hola mundo".toUpperCase()}</h1>
-        <p> Un parrafo</p>
-      </div>
-
-      <div >
-        <h1>{edad}</h1>
-        <p> Un parrafo</p>
-      </div>
+      <Header/>
     </>
   )
 }
 
-export default App
+export default App;
