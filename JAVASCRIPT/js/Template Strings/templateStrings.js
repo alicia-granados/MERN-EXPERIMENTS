1373c81363db50c6f50ef45c3ec7b0620b13c3f0
alicia-granados <aliciagranados35@gmail.com>
2022-08-13 14:47:41 -0500

    feat: :sparkles: Javascript: Template strings

---
+++ JAVASCRIPT/js/Template Strings/templateStrings.js
@@ -0,0 +1,9 @@
+// Template strings
+
+const producto = " Tabñet de 12  pulgadas"
+const precio = 400
+const marca = "orange"
+
+const textoFuncion = () => "este texto proviene de una función"
+
+console.log(`${producto} $${precio} dolares ,marca ${marca}, ${textoFuncion()}`)
