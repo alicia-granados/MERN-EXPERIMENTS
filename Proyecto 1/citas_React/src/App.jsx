11d92cc5f03c09414fa62c038631a82bcf1642bd
alicia-granados <aliciagranados35@gmail.com>
2022-08-13 16:47:44 -0500

    feat: :sparkles: React: Javascript en HTML con JSX

--- Proyecto 1/citas_React/src/App.jsx
+++ Proyecto 1/citas_React/src/App.jsx
@@ -1,9 +1,33 @@
 function App() {
   
+  const sumar = () => {
+    const numero = 5;
+    if(numero > 5){
+      console.log("si es mayor")
+    }else{
+      console.log("no es mayir")
+    }
+    console.log(2 + 2)
+  }
+  sumar();
+
+  const edad = 18;
+
   return (
-    <div className="App">
-      <h1>Hola mundo</h1>
-    </div>
+    <>
+      { 1 + 1 }
+      {edad >= 18 ? 'eres mayor de edad' : 'no eres mayor de edad'}
+      
+      <div >
+        <h1>{"Hola mundo".toUpperCase()}</h1>
+        <p> Un parrafo</p>
+      </div>
+
+      <div >
+        <h1>{edad}</h1>
+        <p> Un parrafo</p>
+      </div>
+    </>
   )
 }
 
