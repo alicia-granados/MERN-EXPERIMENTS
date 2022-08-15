008d93dda3767cad16e629f8caabbb1b81e5cf12
alicia-granados <aliciagranados35@gmail.com>
2022-08-15 10:05:32 -0500

    feat: :sparkles: React: Listado de Pacientes

--- Proyecto 1/citas_React/src/components/Formulario.jsx
+++ Proyecto 1/citas_React/src/components/Formulario.jsx
@@ -1,12 +1,12 @@
 const Formulario = () => {
   return (
-    <div className = "md:w-1/2 lg:w-2/5">
+    <div className = "md:w-1/2 lg:w-2/5 px-12">
 
         <h2 className = "font-black text-3xl text-center">Seguimiento Pacientes</h2>
         
         <p className = "text-lg mt-5 text-center mb-10">
           Añade Pacientes y {''}
-          <span className = "text-indigo-100 font-bold">Administralos</span>
+          <span className = "text-indigo-600 font-bold">Administralos</span>
         </p>
 
         <form className = " bg-white shadow-md rounded-lg py-10 px-5 mb-10">
---
+++ Proyecto 1/citas_React/src/components/Paciente.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Paciente = () => {
+  return (
+    <div className="bg-white shadow-md px-5 py-10 rounded-xl">
+        <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {''}
+          <span className="font-normal normal-case">Hook</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: {''}
+          <span className="font-normal normal-case">Alicia</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Email: {''}
+          <span className="font-normal normal-case">correo@correo.com</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Fecha Alta : {''}
+          <span className="font-normal normal-case">15 de Agosto de 2022</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Síntomas: {''}
+          <span className="font-normal normal-case">Neque porro quisquam est qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit</span>
+        </p>
+    </div>
+  )
+}
+
+export default Paciente
