38384d4e5b33f67fce45b8b6007ce129d4623916
alicia-granados <aliciagranados35@gmail.com>
2022-08-21 11:47:45 -0500

    feat: :sparkles: React: Forma condicional para el formulario

--- Proyecto 1/citas_React/src/components/Formulario.jsx
+++ Proyecto 1/citas_React/src/components/Formulario.jsx
@@ -143,7 +143,7 @@ const Formulario = ({pacientes, setPacientes, paciente}) => {
             type= "submit"
             className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 
             cursor-pointer transition-all"
-            value="Agregar Paciente"
+            value={paciente.id ? "Editar paciente" : "Agregar paciente"}
           />
         </form>
     </div>
