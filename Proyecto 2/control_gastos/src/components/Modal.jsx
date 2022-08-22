7657c8559804e1f8b58fa06fff6290664a87f22c
alicia-granados <aliciagranados35@gmail.com>
2022-08-22 14:14:49 -0500

    feat: :sparkles: React: Añadiendo el formulario de gastos

--- Proyecto 2/control_gastos/src/components/Modal.jsx
+++ Proyecto 2/control_gastos/src/components/Modal.jsx
@@ -21,6 +21,38 @@ const Modal = ({setModal,animarModal,setAnimarModal}) => {
             </div>
             <form className={`formulario ${animarModal ? "animar" : 'cerrar'}`}>
                 <legend>Nuevo gasto</legend>
+                <div className='campo'>
+                    <label htmlFor='nombre'>Nombre Gasto</label>
+                    <input
+                        id='nombre'
+                        type='text'
+                        placeholder='Añade el nombre del gasto'
+                    />
+                </div>
+                <div className='campo'>
+                    <label htmlFor='cantidad'>Cantidad</label>
+                    <input
+                        id='cantidad'
+                        type='number'
+                        placeholder='Añade la cantidad del gasto ej.200'
+                    />
+                </div>
+                <div className='campo'>
+                    <label htmlFor='categoria'>Categoría</label>
+                    <select 
+                        id='categoria'
+                    >
+                        <option value=''>Seleccione</option>
+                        <option value='ahorro'>Ahorro</option>
+                        <option value='comida'>Comida</option>
+                        <option value='casa'>Casa</option>
+                        <option value='gastos'>Gastos varios</option>
+                        <option value='ocio'>Ocio</option>
+                        <option value='salud'>Salud</option>
+                        <option value='sucripciones'>Sucripciones</option>
+                    </select>
+                </div>
+                <input type='submit' value='Añadir Gasto'/>
             </form>
         </div>
     )
