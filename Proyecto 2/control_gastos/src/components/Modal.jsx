2c71a143b14facd969f34390615b0452af8ac47f
alicia-granados <aliciagranados35@gmail.com>
2022-08-22 15:11:44 -0500

    feat: :sparkles: React: creando el state para los gastos

--- Proyecto 2/control_gastos/src/components/Modal.jsx
+++ Proyecto 2/control_gastos/src/components/Modal.jsx
@@ -1,6 +1,12 @@
+import { useState } from 'react'
 import CerrarBtn  from '../img/cerrar.svg'
 
 const Modal = ({setModal,animarModal,setAnimarModal}) => {
+
+    const [nombre, setNombre] = useState('');
+    const [cantidad, setCantidad] = useState('')
+    const [categoria, setCategoria] = useState('')
+
 
     const ocultarModal = () => {
         setAnimarModal(false)
@@ -27,6 +33,8 @@ const Modal = ({setModal,animarModal,setAnimarModal}) => {
                         id='nombre'
                         type='text'
                         placeholder='Añade el nombre del gasto'
+                        value={nombre}
+                        onChange= {e => setNombre(e.target.value)}
                     />
                 </div>
                 <div className='campo'>
@@ -35,12 +43,16 @@ const Modal = ({setModal,animarModal,setAnimarModal}) => {
                         id='cantidad'
                         type='number'
                         placeholder='Añade la cantidad del gasto ej.200'
+                        value={cantidad}
+                        onChange= {e => setCantidad(Number(e.target.value))}
                     />
                 </div>
                 <div className='campo'>
                     <label htmlFor='categoria'>Categoría</label>
                     <select 
                         id='categoria'
+                        value={categoria}
+                        onChange= {e => setCategoria(e.target.value)}
                     >
                         <option value=''>Seleccione</option>
                         <option value='ahorro'>Ahorro</option>
