e73132e4171b7cc1673420a413f9040577aabbc5
alicia-granados <aliciagranados35@gmail.com>
2022-08-22 12:03:34 -0500

    feat: :sparkles: React: Creando el Formulario Presupuesto

--- Proyecto 2/control_gastos/src/App.jsx
+++ Proyecto 2/control_gastos/src/App.jsx
@@ -3,9 +3,14 @@ import Header from './components/Header'
 
 function App() {
 
+  const [presupuesto,setPresupuesto] = useState(0);
+
   return (
     <div className="App">
-      <Header/>
+      <Header
+        presupuesto={presupuesto}
+        setPresupuesto={setPresupuesto}
+      />
     </div>
   )
 }
