6e074ab1d6553ae4d8429074e7fbb8ed7fe4a292
alicia-granados <aliciagranados35@gmail.com>
2022-08-22 12:20:48 -0500

    feat: :sparkles: React: Validando el presupuesto

--- Proyecto 2/control_gastos/src/components/NuevoPresupuesto.jsx
+++ Proyecto 2/control_gastos/src/components/NuevoPresupuesto.jsx
@@ -1,23 +1,36 @@
-import React from 'react'
+import {useState} from 'react';
+import Mensaje from './Mensaje';
 
 const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
-  return (
-    <div className='contenedor-presupuesto contenedor sombra'>
-        <form className='formulario'>
-            <div className='campo'>
-                <label>Definir Presupuesto</label>
-                <input 
-                    className='nuevo-presupuesto'
-                    type='text'
-                    placeholder='Añade tu Presupuesto'
-                    value={presupuesto}
-                    onChange={e => setPresupuesto(e.target.value)}
-                />
-            </div>
-            <input type='submit' value='Añadir'/>
-        </form>
-    </div>
-  )
+    
+    const [mensaje, setMensaje] = useState('');
+
+    const handlePresupuesto = (e) => {
+        e.preventDefault();
+        if(!Number(presupuesto) || Number(presupuesto)< 0){
+            setMensaje('No es un presupuesto válido')
+        }else{
+            setMensaje('Si es un presupuesto válido')
+        }
+    }
+    return (
+        <div className='contenedor-presupuesto contenedor sombra'>
+            <form onSubmit={handlePresupuesto} className='formulario'>
+                <div className='campo'>
+                    <label>Definir Presupuesto</label>
+                    <input 
+                        className='nuevo-presupuesto'
+                        type='text'
+                        placeholder='Añade tu Presupuesto'
+                        value={presupuesto}
+                        onChange={e => setPresupuesto(e.target.value)}
+                    />
+                </div>
+                <input type='submit' value='Añadir'/>
+                {mensaje && <Mensaje tipo='error'>{mensaje}</Mensaje>}
+            </form>
+        </div>
+    )
 }
 
 export default NuevoPresupuesto
