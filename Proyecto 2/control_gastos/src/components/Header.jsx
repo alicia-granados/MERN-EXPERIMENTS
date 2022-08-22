6a03caf6c79827dd7d18c68dc0becf71a3047b9b
alicia-granados <aliciagranados35@gmail.com>
2022-08-22 12:50:27 -0500

    feat: :sparkles: React: Primeros  pason con el componente de control de presupuesto

--- Proyecto 2/control_gastos/src/components/Header.jsx
+++ Proyecto 2/control_gastos/src/components/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import NuevoPresupuesto from './NuevoPresupuesto'
+import ControlPresupuesto from './ControlPresupuesto'
 
 const Header = ({presupuesto, setPresupuesto,isValidPresupuesto, setIsValidPresupuesto}) => {
   return (
@@ -7,7 +8,9 @@ const Header = ({presupuesto, setPresupuesto,isValidPresupuesto, setIsValidPresu
       <header>
           <h1>Planificador de gastos</h1>
           { isValidPresupuesto ? (
-            <p>Control Presupuesto</p>
+            <ControlPresupuesto
+                presupuesto={presupuesto}
+            />
           ): (
             <NuevoPresupuesto
                 presupuesto={presupuesto}
