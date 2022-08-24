804fad5339a5cb2b034a1edca07281e5254839f6
alicia-granados <aliciagranados35@gmail.com>
2022-08-24 10:51:05 -0500

    feat: :sparkles: React: Creando el componente para crear un gasto

--- Proyecto 2/control_gastos/src/App.jsx
+++ Proyecto 2/control_gastos/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import ListadoGastos from './components/ListadoGastos';
 import Modal from './components/Modal';
 import { generarId} from './helpers';
 import IconoNuevoGasto from './img/nuevo-gasto.svg'
@@ -37,13 +38,21 @@ function App() {
         setIsValidPresupuesto={setIsValidPresupuesto}
       />
       {isValidPresupuesto ? (
-        <div className='nuevo-gasto'>
-          <img
-            src={IconoNuevoGasto} 
-            alt='icono nuevo gasto'
-            onClick={handleNuevoGasto}
-          />
-        </div>
+        <>
+          <main>
+            <ListadoGastos
+              gastos={gastos}
+            />
+          </main> 
+          <div className='nuevo-gasto'>
+            <img
+              src={IconoNuevoGasto} 
+              alt='icono nuevo gasto'
+              onClick={handleNuevoGasto}
+            />
+          </div>
+        </>
+
       ): null }
 
       { modal && 
---
+++ Proyecto 2/control_gastos/src/components/Gasto.jsx
@@ -0,0 +1,9 @@
+const Gasto = ({gasto}) => {
+  return (
+    <div>
+      <h1>ka</h1>
+    </div>
+  )
+}
+
+export default Gasto
