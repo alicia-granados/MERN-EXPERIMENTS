d33018b72c2b275842aecb3708b5428eaef7d937
alicia-granados <aliciagranados35@gmail.com>
2022-08-24 17:20:41 -0500

    feat: :sparkles: React: colocando el gasto a editar en el state y en el formulario

--- Proyecto 2/control_gastos/src/components/Gasto.jsx
+++ Proyecto 2/control_gastos/src/components/Gasto.jsx
@@ -24,13 +24,13 @@ const diccionarioIconos ={
     salud: IconoSalud,
     sucripciones: IconoSuscripciones,
 }
-const Gasto = ({gasto}) => {
+const Gasto = ({gasto, setGastoEditar}) => {
 
     const {categoria,nombre, cantidad, id,fecha} = gasto;
 
     const leadingActions =() => (
         <LeadingActions>
-            <SwipeAction onClick={() => console.log('editar')}>
+            <SwipeAction onClick={() => setGastoEditar(gasto)}>
                 Editar
             </SwipeAction>
         </LeadingActions>
