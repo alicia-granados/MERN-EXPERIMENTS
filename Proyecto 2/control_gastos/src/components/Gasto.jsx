33c8206390909b5b5b733ec56e63bd68ac3719dc
alicia-granados <aliciagranados35@gmail.com>
2022-08-24 11:00:16 -0500

    feat: :sparkles: React: Mostrando los gastos

--- Proyecto 2/control_gastos/src/components/Gasto.jsx
+++ Proyecto 2/control_gastos/src/components/Gasto.jsx
@@ -1,9 +1,17 @@
 const Gasto = ({gasto}) => {
-  return (
-    <div>
-      <h1>ka</h1>
-    </div>
-  )
+
+    const {categoria,nombre, cantidad, id} = gasto;
+
+    return (
+        <div className="gasto sombra">
+        <div className="contenido-gasto">
+            <div className="descripcion-gasto">
+                <p className="categoria">{categoria}</p>
+                <p className="nombre-gasto">{nombre}</p>
+            </div>
+        </div>
+        </div>
+    )
 }
 
 export default Gasto
