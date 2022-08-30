cddd4c54018c7a27b2f51b8d371e947e0547390e
alicia-granados <aliciagranados35@gmail.com>
2022-08-29 19:46:40 -0500

    feat: :sparkles: React: Formateando el resultado

--- Proyecto 3/criptos-react/src/components/Resultado.jsx
+++ Proyecto 3/criptos-react/src/components/Resultado.jsx
@@ -1,5 +1,30 @@
 import styled from '@emotion/styled'
 
+const Contenedor = styled.div`
+    color:#fff;
+    font-family:'Lato', sans-serif;
+    display:flex;
+    align-items:center;
+    gap:1rem;
+    margin-top:30px
+`
+const Texto = styled.p`
+    font-size:18px;
+    span{
+        font-weight:700;
+    }
+`
+const Precio = styled.p`
+    font-size:30px;
+    span{
+        font-weight:700;
+    }
+`
+const Imagen = styled.img`
+    display:block;
+    width:100px;
+`
+
 const Resultado = ({resultado}) => {
     const { 
         PRICE, 
@@ -11,14 +36,16 @@ const Resultado = ({resultado}) => {
     } = resultado
 
   return (
-    <div>
-      <p>El precio es de: <span>{PRICE}</span></p>
-      <p>Precio más alto del día: <span>{HIGHDAY}</span></p>
-      <p>Precio más bajo del día: <span>{LOWDAY}</span></p>
-      <p>Variación últimas 24 horas: <span>{CHANGEPCT24HOUR}</span></p>
-      <p>Última actualización: <span>{LASTUPDATE}</span></p>
-
-    </div>
+    <Contenedor>
+        <Imagen src={`https://cryptocompare.com/${IMAGEURL}`} alt='imagen cripto'/>
+        <div>
+            <Precio>El precio es de: <span>{PRICE}</span></Precio>
+            <Texto>Precio más alto del día: <span>{HIGHDAY}</span></Texto>
+            <Texto>Precio más bajo del día: <span>{LOWDAY}</span></Texto>
+            <Texto>Variación últimas 24 horas: <span>{CHANGEPCT24HOUR}</span></Texto>
+            <Texto>Última actualización: <span>{LASTUPDATE}</span></Texto>
+        </div>
+    </Contenedor>
   )
 }
 
