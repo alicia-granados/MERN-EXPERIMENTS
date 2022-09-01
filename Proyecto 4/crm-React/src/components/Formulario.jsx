c2a5026987f091cc6be87911bb393aa83c03d524
alicia-granados <aliciagranados35@gmail.com>
2022-08-31 21:18:28 -0500

    feat: :sparkles: React: Valores iniciales para el formulario

--- Proyecto 4/crm-React/src/components/Formulario.jsx
+++ Proyecto 4/crm-React/src/components/Formulario.jsx
@@ -5,77 +5,94 @@ const Formulario = () => {
     <div className='bg-white mt-10 px-5 py-10 rounded-md shadow-md md:w-3/4 mx-auto'>
         <h1 className='text-gray-600 font-bold text-xl uppercase text-center'>Agregar Cliente</h1>
 
-        <Formik>
-            <Form className='mt-10'>
-                <div className='mb-4 '>
-                    <label
-                        className='text-gray-800'
-                        htmlFor='nombre'
-                    >Nombre:</label>
-                    <Field
-                        id='nombre'
-                        placeholder='Nombre del Cliente'
-                        type='text'
-                        className='mt-2 block w-full bg-gray-50'
-                    />
-                </div>
-                <div className='mb-4 '>
-                    <label
-                        className='text-gray-800'
-                        htmlFor='empresa'
-                    >Empresa:</label>
-                    <Field
-                        id='empresa'
-                        placeholder='Empresa del Cliente'
-                        type='text'
-                        className='mt-2 block w-full bg-gray-50'
-                    />
-                </div>
-                <div className='mb-4 '>
-                    <label
-                        className='text-gray-800'
-                        htmlFor='email'
-                    >Email:</label>
-                    <Field
-                        id='email'
-                        placeholder='Email del Cliente'
-                        type='email'
-                        className='mt-2 block w-full bg-gray-50'
-                    />
-                </div>
-                <div className='mb-4 '>
-                    <label
-                        className='text-gray-800'
-                        htmlFor='telefono'
-                    >Telefono:</label>
-                    <Field
-                        id='telefono'
-                        placeholder='Telefono del Cliente'
-                        type='tel'
-                        className='mt-2 block w-full bg-gray-50'
-                    />
-                </div>
-                <div className='mb-4 '>
-                    <label
-                        className='text-gray-800'
-                        htmlFor='notas'
-                    >Notas:</label>
-                    <Field
-                        as='textarea'
-                        id='notas'
-                        placeholder='Notas del Cliente'
-                        type='text'
-                        className='mt-2 block w-full bg-gray-50 h-30'
-                    />
-                </div>
+        <Formik
+            initialValues={{
+                nombre:'',
+                empresa:'',
+                email: '',
+                telefono:'',
+                notas:''
+            }}
+        >
+            { () => (
+                <Form className='mt-10'>
+                    <div className='mb-4 '>
+                        <label
+                            className='text-gray-800'
+                            htmlFor='nombre'
+                        >Nombre:</label>
+                        <Field
+                            id='nombre'
+                            name='nombre'
+                            placeholder='Nombre del Cliente'
+                            type='text'
+                            className='mt-2 block w-full bg-gray-50'
+                        />
+                    </div>
+                    <div className='mb-4 '>
+                        <label
+                            className='text-gray-800'
+                            htmlFor='empresa'
+                        >Empresa:</label>
+                        <Field
+                            id='empresa'
+                            name='empresa'
+                            placeholder='Empresa del Cliente'
+                            type='text'
+                            className='mt-2 block w-full bg-gray-50'
+                        />
+                    </div>
+                    <div className='mb-4 '>
+                        <label
+                            className='text-gray-800'
+                            htmlFor='email'
+                        >Email:</label>
+                        <Field
+                            id='email'
+                            name='email'
+                            placeholder='Email del Cliente'
+                            type='email'
+                            className='mt-2 block w-full bg-gray-50'
+                        />
+                    </div>
+                    <div className='mb-4 '>
+                        <label
+                            className='text-gray-800'
+                            htmlFor='telefono'
+                        >Telefono:</label>
+                        <Field
+                            id='telefono'
+                            name='telefono'
+                            placeholder='Telefono del Cliente'
+                            type='tel'
+                            className='mt-2 block w-full bg-gray-50'
+                        />
+                    </div>
+                    <div className='mb-4 '>
+                        <label
+                            className='text-gray-800'
+                            htmlFor='notas'
+                        >Notas:</label>
+                        <Field
+                            as='textarea'
+                            id='notas'
+                            name='notas'
+                            placeholder='Notas del Cliente'
+                            type='text'
+                            className='mt-2 block w-full bg-gray-50 h-30'
+                        />
+                    </div>
 
-                <button 
-                    className='mt-5 w-full bg-blue-800 p-3 text-white font-bold text-lg'
-                >
-                    Agregar
-                </button>
+                    <button 
+                        className='mt-5 w-full bg-blue-800 p-3 text-white font-bold text-lg'
+                    >
+                        Agregar
+                    </button>
 
-            </Form>
+                </Form>
+
+            )}
+            
         </Formik>    
     
     </div>
