e87d80726c7f7c4395788c6a6e11220a22694301
alicia-granados <aliciagranados35@gmail.com>
2022-08-31 23:08:49 -0500

    feat: :sparkles: React:  Añadiendo clientes  desde el formulario

--- Proyecto 4/crm-React/src/components/Formulario.jsx
+++ Proyecto 4/crm-React/src/components/Formulario.jsx
@@ -20,8 +20,22 @@ const Formulario = () => {
             .typeError('El número no es válido'),
     })
 
-    const handleSubmit = (valores) => {
-        console.log(valores)
+    const handleSubmit = async (valores) => {
+        try{
+            const url = 'http://localhost:4000/clientes'
+            const respuesta = await fetch(url,{
+                method:'POST',
+                body: JSON.stringify(valores),
+                headers:{
+                    'Content-Type': 'application/json'
+                }
+            })
+            console.log(respuesta)
+            const resultado = await respuesta.json()
+            console.log(resultado)
+        }catch(error){
+            console.log(error)
+        }
     }
 
     return (
