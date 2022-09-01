06f3240658d46cc52e3b1bfb18a4fce157880112
alicia-granados <aliciagranados35@gmail.com>
2022-08-31 23:37:01 -0500

    feat: :sparkles: React: consultar la API para obtener los clientes

--- Proyecto 4/crm-React/src/paginas/Inicio.jsx
+++ Proyecto 4/crm-React/src/paginas/Inicio.jsx
@@ -1,6 +1,22 @@
-import React from 'react'
+import {useState,useEffect} from 'react'
 
 const Inicio = () => {
+
+  const[clientes,setClientes] = useState([])
+  
+  useEffect(()=>{
+    const obtenerClientesAPI = async() => {
+      try{
+        const url = 'http://localhost:4000/clientes'
+        const respuesta = await fetch(url)
+        const resultado = await respuesta.json()
+        setClientes(resultado)
+      }catch(error){
+        console.log(error)
+      }
+    }
+    obtenerClientesAPI()
+  },[])
   return (
     <div>
       <h1>Desde inicio.jsx</h1>
