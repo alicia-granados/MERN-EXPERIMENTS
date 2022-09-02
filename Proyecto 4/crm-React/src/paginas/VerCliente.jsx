f60029cdfe1cc429560822f5206d3a3a9654ed56
alicia-granados <aliciagranados35@gmail.com>
2022-09-01 20:21:56 -0500

    feat: :sparkles: React: detectando  cuando no hay resultados

--- Proyecto 4/crm-React/src/paginas/VerCliente.jsx
+++ Proyecto 4/crm-React/src/paginas/VerCliente.jsx
@@ -27,40 +27,41 @@ const VerCliente = () => {
     },[])
 
     return (
+        Object.keys(cliente).length === 0 ? <p>No hay resultados</p> : (
+            <div>
 
-        <div>
-
-            <h1 className='font-block text-4xl text-blue-900 '>Ver Cliente: {nombre}</h1>
-            <p className='mt-3'>Información cliente</p>
+                <h1 className='font-block text-4xl text-blue-900 '>Ver Cliente: {nombre}</h1>
+                <p className='mt-3'>Información cliente</p>
 
 
-            <p className='text-4xl text-gray-800 mt-10 '> 
-                <span className='uppercase font-bold'>Cliente: </span>
-                {nombre}
-            </p>
-            <p className='text-2xl mt-4  text-gray-600 '> 
-                <span className='uppercase font-bold'>Email: </span>
-                {email}
-            </p>
-            {telefono && (
-                <p className='text-2xl mt-4  text-gray-600 '> 
-                    <span className='uppercase font-bold'>Teléfono: </span>
-                    {telefono}
+                <p className='text-4xl text-gray-800 mt-10 '> 
+                    <span className='uppercase font-bold'>Cliente: </span>
+                    {nombre}
                 </p>
-            )}
-
-            <p className='text-2xl mt-4  text-gray-600 '> 
-                <span className='uppercase font-bold'>Empresa: </span>
-                {empresa}
-            </p>
-            { notas && (
                 <p className='text-2xl mt-4  text-gray-600 '> 
-                    <span className='uppercase font-bold'>Notas: </span>
-                    {notas}
+                    <span className='uppercase font-bold'>Email: </span>
+                    {email}
                 </p>
-            )}
+                {telefono && (
+                    <p className='text-2xl mt-4  text-gray-600 '> 
+                        <span className='uppercase font-bold'>Teléfono: </span>
+                        {telefono}
+                    </p>
+                )}
 
-        </div>
+                <p className='text-2xl mt-4  text-gray-600 '> 
+                    <span className='uppercase font-bold'>Empresa: </span>
+                    {empresa}
+                </p>
+                { notas && (
+                    <p className='text-2xl mt-4  text-gray-600 '> 
+                        <span className='uppercase font-bold'>Notas: </span>
+                        {notas}
+                    </p>
+                )}
+
+            </div>
+        )
     )
 }
 
