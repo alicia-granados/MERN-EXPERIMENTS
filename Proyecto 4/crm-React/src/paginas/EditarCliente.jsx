daceeb8bd36b394efa5fe7a82d5883a6bc9204fd
alicia-granados <aliciagranados35@gmail.com>
2022-09-01 21:57:06 -0500

    feat: :sparkles: React: primeros pasos con la edición de clientes

--- Proyecto 4/crm-React/src/paginas/EditarCliente.jsx
+++ Proyecto 4/crm-React/src/paginas/EditarCliente.jsx
@@ -1,9 +1,36 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import {useParams} from 'react-router-dom'
+import Formulario from "../components/Formulario"
 
 const EditarCliente = () => {
+  const [cliente,setCliente] = useState({})
+
+  const [cargando,setCargando] = useState(true)
+
+  const {id}= useParams()
+
+  useEffect(()=>{
+    const obtenerClienteAPI = async () => {
+      try{
+        const url = `http://localhost:4000/clientes/${id}`
+        const respuesta = await fetch(url)
+        const resultado = await respuesta.json()
+        setCliente(resultado)
+      }catch(error){
+        console.log(error)
+      }
+        setCargando(false)
+    }
+    obtenerClienteAPI()
+  },[])
+
   return (
     <div>
-      <h1>Desde EditarCliente.jsx</h1>
+      <h1 className='font-block text-4xl text-blue-900 '>Editar Cliente</h1>
+      <p className='mt-3'>Utiliza este formulario para editar un cliente</p>
+      <Formulario
+        cliente={cliente}
+      />
     </div>
   )
 }
