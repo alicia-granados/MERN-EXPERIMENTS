cffec444c01ae7a0a05fc8537acb35f20d707646
alicia-granados <aliciagranados35@gmail.com>
2022-09-01 20:11:49 -0500

    feat: :sparkles: React: Mostrando la información de un cliente por su ID

--- Proyecto 4/crm-React/src/paginas/VerCliente.jsx
+++ Proyecto 4/crm-React/src/paginas/VerCliente.jsx
@@ -1,12 +1,61 @@
+import { useEffect, useState } from 'react'
 import {useParams} from 'react-router-dom'
 
 const VerCliente = () => {
+    const [cliente,setCliente] = useState({})
+
+    const {nombre,empresa,email,telefono,notas} = cliente
+
     const {id}= useParams()
-    console.log(id)
+
+    useEffect(()=>{
+        const obtenerClienteAPI = async () => {
+            try{
+                const url = `http://localhost:4000/clientes/${id}`
+                const respuesta = await fetch(url)
+                const resultado = await respuesta.json()
+                setCliente(resultado)
+              }catch(error){
+                console.log(error)
+              }
+        }
+        obtenerClienteAPI()
+    },[])
+
     return (
 
         <div>
-        
+
+            <h1 className='font-block text-4xl text-blue-900 '>Ver Cliente: {nombre}</h1>
+            <p className='mt-3'>Información cliente</p>
+
+
+            <p className='text-4xl text-gray-800 mt-10 '> 
+                <span className='uppercase font-bold'>Cliente: </span>
+                {nombre}
+            </p>
+            <p className='text-2xl mt-4  text-gray-600 '> 
+                <span className='uppercase font-bold'>Email: </span>
+                {email}
+            </p>
+            {telefono && (
+                <p className='text-2xl mt-4  text-gray-600 '> 
+                    <span className='uppercase font-bold'>Teléfono: </span>
+                    {telefono}
+                </p>
+            )}
+
+            <p className='text-2xl mt-4  text-gray-600 '> 
+                <span className='uppercase font-bold'>Empresa: </span>
+                {empresa}
+            </p>
+            { notas && (
+                <p className='text-2xl mt-4  text-gray-600 '> 
+                    <span className='uppercase font-bold'>Notas: </span>
+                    {notas}
+                </p>
+            )}
+
         </div>
     )
 }
