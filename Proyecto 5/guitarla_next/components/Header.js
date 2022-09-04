f244171ef71a37e506695869c99f04d0cc342e62
alicia-granados <aliciagranados35@gmail.com>
2022-09-04 17:37:42 -0500

    feat: :sparkles: Next: El componente Image de Next.js

--- Proyecto 5/guitarla_next/components/Header.js
+++ Proyecto 5/guitarla_next/components/Header.js
@@ -1,13 +1,20 @@
 import Link from "next/link"
+import Image from 'next/image'
 import styles from '../styles/Header.module.css'
 
 const Header = () => {
   return (
     <header className={styles.header}>
       <div className='contenedor'>
-        <div>
+        <Link href='/'>
+            <Image 
+                src='/img/logo.svg'
+                width={400} 
+                height={100}
+                alt='Imagen logo'
+            />
+        </Link>
 
-        </div>
         <nav className={styles.navegacion}>
             <Link href= '/'>Inicio</Link>
             <Link href= '/nosotros'>Nosotros</Link>
