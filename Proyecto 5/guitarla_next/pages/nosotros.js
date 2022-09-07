be88fb9c91514e96801f24f2c4fca1baf509a9a2
alicia-granados <aliciagranados35@gmail.com>
2022-09-06 22:33:06 -0500

    feat: :sparkles: Next:  creando  la página de nosotros

--- Proyecto 5/guitarla_next/pages/nosotros.js
+++ Proyecto 5/guitarla_next/pages/nosotros.js
@@ -1,12 +1,38 @@
+import Image from 'next/image'
 import Layout from '../components/Layout'
+import styles from '../styles/Nosotros.module.css'
 
 const Nosotros = () => {
   return (
     <div>
         <Layout
-            pagina='Nosotros'
+          pagina='Nosotros'
         >
-            <h1>Desde nosotros</h1>
+          <main className='contenedor'>
+            <h2 className='heading'>Nosotros</h2>
+            <div className={styles.contenido}>
+              <Image
+                layout='responsive'
+                width={600} height={450} 
+                src='/img/nosotros.jpg'
+                alt='Imagen sobre nosotros'
+              />
+              <div>
+                
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
+                  et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+                  aliquip ex ea commodo consequat. 
+                </p>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore 
+                  et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut 
+                  aliquip ex ea commodo consequat. 
+                </p>
+              </div>
+            </div>
+          </main>
+            
         </Layout> 
     </div>
   )
