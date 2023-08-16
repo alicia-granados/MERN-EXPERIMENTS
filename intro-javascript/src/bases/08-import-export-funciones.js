89de5c74af074f663763ff97602c9cf018a65cb5
alicia-granados <aliciagranados35@gmail.com>
2023-08-15 20:52:11 -0600

    ✨ feat:Promesas

--- intro-javascript/src/bases/08-import-export-funciones.js
+++ intro-javascript/src/bases/08-import-export-funciones.js
@@ -1,11 +1,11 @@
 //console.log('??')
 //import  {heroes} from './data/heroes';
-import  {heroes, owners} from '../data/heroes';
+import  heroes from '../data/heroes';
 console.log(heroes);
 
-const getHeroeById  = (id) => heroes.find((heroe) => heroe.id === id);
+export const getHeroeById  = (id) => heroes.find((heroe) => heroe.id === id);
 
-console.log(getHeroeById(2))
+//console.log(getHeroeById(2))
 
-const getHeroesByOwner = (owner) => heroes.filter((heroe) => heroe.owner === owner);
-console.log(getHeroesByOwner('DC'))
+export const getHeroesByOwner = (owner) => heroes.filter((heroe) => heroe.owner === owner);
+//console.log(getHeroesByOwner('DC'))
