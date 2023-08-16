af39f4c45e359b0cfe883e467f82d35a9d01b0ff
alicia-granados <aliciagranados35@gmail.com>
2023-08-15 18:24:09 -0600

    ✨ feat:variables y constantes

---
+++ intro-javascript/src/bases/01-const-let.js
@@ -0,0 +1,16 @@
+//variables y constantes
+// var no se debe de usar.....
+const nombre  = 'fernando';
+const apellido = 'Herrera';
+
+let valorDado = 5;
+valorDado = 4;
+
+console.log(nombre, apellido, valorDado)
+
+if ( true ){
+    let valorDado = 6;
+    console.log(valorDado);
+}
+
+console.log(valorDado);
