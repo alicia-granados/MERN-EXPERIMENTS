476ad90e915823f4780bfb373071ecc3899f9b73
alicia-granados <aliciagranados35@gmail.com>
2023-08-15 19:42:19 -0600

    ✨ feat:import, export, y funciones comunes de arreglos

---
+++ intro-javascript/src/bases/08-import-export-funciones.js
@@ -0,0 +1,10 @@
+//console.log('??')
+import  {heroes} from './data/heroes';
+console.log(heroes);
+
+const getHeroeById  = (id) => heroes.find((heroe) => heroe.id === id);
+
+console.log(getHeroeById(2))
+
+const getHeroesByOwner = (owner) => heroes.filter((heroe) => heroe.owner === owner);
+console.log(getHeroesByOwner('DC'))
