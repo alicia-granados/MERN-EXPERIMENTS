8539920e1505318d8413b4b515d9c0a2b792cbcb
alicia-granados <aliciagranados35@gmail.com>
2023-08-16 20:51:07 -0600

    ✨ feat:useState - Hook

--- 06-counter-react-vite/src/CounterApp.jsx
+++ 06-counter-react-vite/src/CounterApp.jsx
@@ -1,14 +1,18 @@
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 const CounterApp = ({value}) => {
+
+    const [counter,setCounter] = useState(value)
     const  handleAdd  = (event) =>  { 
-        console.log('+1');
-        value+=1
+        //console.log(event);
+        setCounter(counter+1);
+        //setCounter( (c) =>  c+ 1)
     }
   return (
     <>
       <h1>Counter App</h1>
-      <h2> {value} </h2>
+      <h2> {counter} </h2>
 
         <button onClick={  handleAdd }>
             +1
