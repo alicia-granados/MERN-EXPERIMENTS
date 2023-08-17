b29ccd63433a48ca0833ef1c54d8333346aa101b
alicia-granados <aliciagranados35@gmail.com>
2023-08-16 20:03:34 -0600

    ✨ feat:comunicacion entre componentes  - Props

--- 06-counter-react-vite/src/FirstApp.jsx
+++ 06-counter-react-vite/src/FirstApp.jsx
@@ -6,9 +6,11 @@ const newMessage2 ={
 const saludo = () =>{
     return 'Hola como estas'
 }
-const FirstApp = () => {
+const FirstApp = ( {title = 'Hola soy fernando'} ) => {
+    //console.log(props)
     return (
         <>
+        <h3>{/*props.title*/ title}</h3>
         <h2> { 1 + 1 } </h2>
         <p>{newMessage} </p>
         <code>{ JSON.stringify(newMessage2)} </code>
--- 06-counter-react-vite/src/main.jsx
+++ 06-counter-react-vite/src/main.jsx
@@ -6,6 +6,6 @@ import './styles.css'
 ReactDOM.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
         <HelloWorldApp />
-        <FirstApp/>
+        <FirstApp title='hola, soy goku' />
     </React.StrictMode>
 );
