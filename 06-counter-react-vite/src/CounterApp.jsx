3064063850bbf6f3defb5aed4d105bdacd42ff78
alicia-granados <aliciagranados35@gmail.com>
2023-08-16 20:42:06 -0600

    ✨ feat:evento click  eventos en general

--- 06-counter-react-vite/src/CounterApp.jsx
+++ 06-counter-react-vite/src/CounterApp.jsx
@@ -1,10 +1,18 @@
 import PropTypes from 'prop-types';
 
 const CounterApp = ({value}) => {
+    const  handleAdd  = (event) =>  { 
+        console.log('+1');
+        value+=1
+    }
   return (
     <>
       <h1>Counter App</h1>
       <h2> {value} </h2>
+
+        <button onClick={  handleAdd }>
+            +1
+        </button>
     </>
   )
 }
