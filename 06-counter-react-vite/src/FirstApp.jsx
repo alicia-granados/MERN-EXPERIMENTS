4073f7e52899685668f0b58d509184d77d6ffa29
alicia-granados <aliciagranados35@gmail.com>
2023-08-16 19:48:21 -0600

    ✨ feat:impresion de variables   en el HTML

--- 06-counter-react-vite/src/FirstApp.jsx
+++ 06-counter-react-vite/src/FirstApp.jsx
@@ -1,10 +1,21 @@
+const newMessage= 'Fernando';
+const newMessage2 ={
+    message : 'hola mundo',
+    title : 'Fernando'
+}
+const saludo = () =>{
+    return 'Hola como estas'
+}
 const FirstApp = () => {
-  return (
-    <>
-      First App
-      <p>Soy un subtitulo</p>
-    </>
-  )
+    return (
+        <>
+        <h2> { 1 + 1 } </h2>
+        <p>{newMessage} </p>
+        <code>{ JSON.stringify(newMessage2)} </code>
+        <p>Soy un subtitulo</p>
+        <h1>{saludo()}</h1>
+        </>
+    )
 }
 
 export default FirstApp
