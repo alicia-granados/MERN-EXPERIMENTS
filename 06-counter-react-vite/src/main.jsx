f05557b89f4e82862314146e986983105a53170d
alicia-granados <aliciagranados35@gmail.com>
2023-08-16 20:26:04 -0600

    ✨ feat: componente counterapp

--- 06-counter-react-vite/src/main.jsx
+++ 06-counter-react-vite/src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client';
 import HelloWorldApp from './HelloWorldApp';
 import FirstApp from './FirstApp';
+import CounterApp from './CounterApp';
 import './styles.css'
 ReactDOM.createRoot( document.getElementById('root') ).render(
     <React.StrictMode>
         <HelloWorldApp />
         <FirstApp title='hola, soy goku' subtitle= {12} />
+        <CounterApp value={45}/>
     </React.StrictMode>
 );
