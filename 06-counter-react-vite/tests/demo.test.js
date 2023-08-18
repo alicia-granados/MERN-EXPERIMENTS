024feecb95d424c601d1e824d730176dc10f6844
alicia-granados <aliciagranados35@gmail.com>
2023-08-17 18:19:55 -0600

    ✨ feat:Jest - Expect - toBe

--- 06-counter-react-vite/tests/demo.test.js
+++ 06-counter-react-vite/tests/demo.test.js
@@ -1,5 +1,12 @@
-test('Esta prueba no debe de fallar',() =>{
-    if( 0 === 1 ) {
-        throw new Error('No se puede dividir entre cero')
-    }
-})
+describe('Pruebas en <DemoComponent/>', () => {
+    
+    test('Esta prueba no debe de fallar',() =>{
+        // 1.  inicializacion
+        const message1 = 'Hola mundo';
+        //2. estimulo
+        const message2 = message1.trim();
+        // 3. Observar el comportamiento ... esperao¿do
+        expect(message1).toBe(message2);
+    });
+
+});
