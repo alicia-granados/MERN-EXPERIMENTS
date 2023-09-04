0674a4f014f79437b91add9268056bd791406849
alicia-granados <aliciagranados35@gmail.com>
2023-09-04 12:56:48 -0600

    ✨ feat:Probar FirstApp

--- 06-counter-react-vite/tests/FirstApp.test.jsx
+++ 06-counter-react-vite/tests/FirstApp.test.jsx
@@ -5,6 +5,19 @@ import FirstApp from "../src/FirstApp"
 describe('Pruebas en <FirstApp/>', () => {
   test('debde de hacer match con el snapshot',() =>{
     const tittle='Hola soy goku'
-    render(<FirstApp title={tittle}/>)
+    const {container} = render(<FirstApp title={tittle}/>)
+    expect(container).toMatchSnapshot();
+
+  })
+
+  test('debde de mostrar el titulo en un h1',() =>{
+    const tittle='Hola soy goku'
+    const {container,getByText } = render(<FirstApp title={tittle}/>)
+    expect(getByText(tittle)).toBeTruthy();
+
+    const h1 = container.querySelector('h1');
+    //expect(h1.innerHTML).toBe(tittle) toma espacios
+    expect(h1.innerHTML).toContain(tittle);
+    
   })
 })
