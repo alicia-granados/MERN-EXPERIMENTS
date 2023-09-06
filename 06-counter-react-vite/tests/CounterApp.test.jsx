a2fc7ed82ea118da5761c280f03131c3cb6cc067
alicia-granados <aliciagranados35@gmail.com>
2023-09-05 19:46:33 -0600

    ✨ feat:pruebas básicas del CounterApp

---
+++ 06-counter-react-vite/tests/CounterApp.test.jsx
@@ -0,0 +1,16 @@
+/** @jest-environment jsdom */
+import { render,screen } from "@testing-library/react"
+import CounterApp from "../src/CounterApp"
+
+describe('Pruebas en <CounterApp/>', () => {
+    const initialValue= 10;
+    test('debe de hacer match con el shapshot',() =>{
+        const {container } = render(<CounterApp value ={initialValue}/>)
+        expect ( container ).toMatchSnapshot();
+    });
+    test('debe de mostrar el valor inicial de 100',() =>{
+        render(<CounterApp value ={100}/>)
+        expect ( screen.getByText(100)).toBeTruthy();
+        //expect(screen.getAllByRole('heading',{level:2}).innerHTML).toContain('100')
+    });
+})
