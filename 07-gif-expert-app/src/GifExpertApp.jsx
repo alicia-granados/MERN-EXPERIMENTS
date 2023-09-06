c94233ae31129a39d1f7500991b23c7649560d78
alicia-granados <aliciagranados35@gmail.com>
2023-09-06 12:10:20 -0600

    ✨ feat:Creando una lista de categorias

--- 07-gif-expert-app/src/GifExpertApp.jsx
+++ 07-gif-expert-app/src/GifExpertApp.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
+
 const GifExpertApp = () => {
+    const [categories, setCategories] = useState(['One Punch']);
+    //console.log(categories);
   return (
     <>
         { /*titulo*/ }
         <h1>GifExpertApp</h1>
+
         { /*input*/ }
+        <ol>
+            {categories.map(category =>{
+                return <li key={category} > {category}</li>
+            })}
+            
+        </ol>  
+
         { /*listado de gif - git item*/ }
     </>
   )
