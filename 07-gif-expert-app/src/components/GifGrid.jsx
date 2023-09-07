761c6091f3e8a2fb28e4a970ec8c59a7e19c072b
alicia-granados <aliciagranados35@gmail.com>
2023-09-06 22:23:16 -0600

    ✨ feat:Custom Hook - useFectchGifs

--- 07-gif-expert-app/src/components/GifGrid.jsx
+++ 07-gif-expert-app/src/components/GifGrid.jsx
@@ -1,18 +1,13 @@
 import { useEffect, useState } from "react";
 import GifItem from "./GifItem";
-import { getGifs } from "../helpers/getGifs";
+import useFetchGifs from "../hooks/useFetchGifs";
+//import { getGifs } from "../helpers/getGifs";
+
 
 const GifGrid = ({category}) => {
-  const [images,setImages] = useState([]);
 
-  const getImages = async  () =>{
-    const newImages = await getGifs(category);
-    setImages(newImages);
-  }
-  useEffect(() => {
-    getImages();
-  },[]);
-  
+  const {images,isLoading}= useFetchGifs(category);
+  //console.log({images,isLoading});
   return (
     <>
       <h3>{category}</h3>
--- 07-gif-expert-app/src/components/GifItem.jsx
+++ 07-gif-expert-app/src/components/GifItem.jsx
@@ -1,5 +1,5 @@
 const GifItem = ({title, url}) => {
-    console.log({title,url})
+    //console.log({title,url})
   return (
     <div className="card">
       <img src={url} alt={title}/>
