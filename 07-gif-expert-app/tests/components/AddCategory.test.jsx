7061901e117879d964b2797a6cb75a21f952ef70
alicia-granados <aliciagranados35@gmail.com>
2023-09-07 20:20:26 -0600

    🧪 test:Jest Functions

--- 07-gif-expert-app/tests/components/AddCategory.test.jsx
+++ 07-gif-expert-app/tests/components/AddCategory.test.jsx
@@ -8,4 +8,37 @@ describe('Pruebas en <AddCategory/>', () => {
         fireEvent.input(input, {target: {value:'Saitana'}});
         expect(input.value).toBe('Saitana');
     });
+
+    test('debe de llamar onNewCategory si el input tiene un valor', () =>{
+        
+        const inputValue= 'Saitana';
+        const onNewCategory = jest.fn();
+
+        render(<AddCategory onNewCategory={ onNewCategory }/>);
+        
+        const input = screen.getByRole('textbox');
+        const form= screen.getByRole('form');
+
+        fireEvent.input(input, {target: {value: inputValue}});
+        fireEvent.submit(form);
+        
+        expect(input.value).toBe('');
+        //sea llamada la funcion  e solo una vez
+        expect(onNewCategory).toHaveBeenCalled();
+        expect(onNewCategory).toHaveBeenCalledTimes(1);
+        expect(onNewCategory).toHaveBeenCalledWidth(inputValue);
+        
+    });
+
+    test('NO debe de llamar onNewCategory si el input NO tiene un valor', () =>{
+        
+        const onNewCategory = jest.fn();
+        render(<AddCategory onNewCategory={ onNewCategory }/>);
+        const form= screen.getByRole('form');
+        fireEvent.submit(form);
+        
+        expect(onNewCategory).toHaveBeenCalledTimes(0);
+        expect(onNewCategory).not.toHaveBeenCalled();
+    });
+
 })
