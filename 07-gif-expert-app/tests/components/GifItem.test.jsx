152ac00c420dadd4635ac8c6858a93cafdfc1b56
alicia-granados <aliciagranados35@gmail.com>
2023-09-07 19:15:58 -0600

    🧪 test:Pruebas de componente GifItem

--- 07-gif-expert-app/tests/components/GifItem.test.jsx
+++ 07-gif-expert-app/tests/components/GifItem.test.jsx
@@ -1,5 +1,5 @@
 /** @jest-environment jsdom */
-import { render } from "@testing-library/react"
+import { render , screen } from "@testing-library/react"
 import {GifItem} from "../../src/components";
 
 describe('Pruebas en <GifItem/>', () => {
@@ -12,5 +12,19 @@ describe('Pruebas en <GifItem/>', () => {
         expect ( container ).toMatchSnapshot();
     });
 
+    test('debe de mostrar la imagen con el url y el alt indicado',() =>{
+        render(<GifItem title={title} url = {url}/>)
+        //screen.edub();
+        //expect(screen.getByRole('img').src).toBe(url);
+        //expect(screen.getByRole('img').alt).toBe(title);
+        const {src,alt } = screen.getByRole('img');
+        expect(src).toBe(url);
+        expect(alt).toBe(title);
+    });
+
+    test('debe de mostrar el titulo en el componente',() =>{
+        render(<GifItem title={title} url = {url}/>);
+        expect(screen.getByText(title)).toBeTruthy();
+    });
 
 })
